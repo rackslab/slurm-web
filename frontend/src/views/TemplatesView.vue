a13991a3dcb8bd5d00a2ea5b7a2c11ca821b3755
Florian Lespérant <florian.lesperant@rackslab.io>
2024-06-11 10:29:43 +0200

    feat(pr): Add Templates and SubmitNewJob views
    - Added navigation buttons and descriptive text to each view.
    - Included back navigation to 'jobs' in both views.

---
+++ frontend/src/views/TemplatesView.vue
@@ -0,0 +1,38 @@
+<!--
+  Copyright (c) 2023-2024 Rackslab
+
+  This file is part of Slurm-web.
+
+  SPDX-License-Identifier: GPL-3.0-or-later
+-->
+
+<script setup lang="ts">
+import ClusterMainLayout from '@/components/ClusterMainLayout.vue'
+import { ChevronLeftIcon } from '@heroicons/vue/20/solid'
+
+const props = defineProps({
+  cluster: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <ClusterMainLayout :cluster="props.cluster">
+    <router-link :to="{ name: 'jobs' }"
+      ><button
+        type="button"
+        class="mb-16 mt-8 inline-flex items-center gap-x-2 rounded-md bg-slurmweb px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-slurmweb-dark focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-slurmweb-dark"
+      >
+        <ChevronLeftIcon class="-ml-0.5 h-5 w-5" aria-hidden="true" />
+        Back to resources
+      </button></router-link
+    >
+
+    <div class="text-center">
+      <p class="text-lg font-bold">Manage templates</p>
+      <p>Edit or create jobs templates</p>
+    </div>
+  </ClusterMainLayout>
+</template>
