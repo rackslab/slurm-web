f4524abafc48045324305c108b858b6976b7d975
Florian Lespérant <florian.lesperant@rackslab.io>
2024-06-13 14:35:17 +0200

    feat(gateway): Create route to get data from agent
    This new route gets templates data from the agent.

--- slurmweb/apps/gateway.py
+++ slurmweb/apps/gateway.py
@@ -53,6 +53,7 @@ class SlurmwebAppGateway(SlurmwebWebApp, RFLTokenizedWebApp):
         SlurmwebAppRoute("/api/agents/<cluster>/qos", views.qos),
         SlurmwebAppRoute("/api/agents/<cluster>/reservations", views.reservations),
         SlurmwebAppRoute("/api/agents/<cluster>/accounts", views.accounts),
+        SlurmwebAppRoute("/api/agents/<cluster>/templates", views.templates),
         SlurmwebAppRoute(
             "/api/agents/<cluster>/racksdb/<path:query>",
             views.racksdb,
